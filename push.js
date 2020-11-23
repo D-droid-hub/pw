829ed6320773b00eeb01055caecbfd827f620d21
Avnish <adishu.deo@gmail.com>
2020-11-23 08:30:08 +0530

    Fixed

--- push.js
+++ push.js
@@ -5,11 +5,11 @@ let vapidKeys = {
     privateKey: 'u8oE2BlRlZwEWDj5iUg4ck2TkNKeK_4ZoxjZc-t5u7M'
 }
 
-push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails('mailto:adishu.deo@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/czlwwhxnENY:APA91bFmKIikrPkVIyqNK7w7is2uruTAmtPIYM_Pgqw3eUPshVgsc0-VB7IBuy8hNBWTAIw3W952zQfRhot2jAecCDH70rRb_CpzQkQOKLNSSukk6k7D4lmub_OllDjNfp34tf1svckf",
- "expirationTime": null, 
- "keys": { "p256dh": "BJovBQ7yPyn5jIywUTY1S1qEsKJScbZp9Hnb0feGFcwwV8PNxhknAJ5QPIhRlR0_k5gWNHl0Yqwnl6LiCcwKnts", 
- "auth": "JK5uUic6jmOI6DJ7bo8AyA" } }
+let sub ={"endpoint":"https://fcm.googleapis.com/fcm/send/czlwwhxnENY:APA91bFmKIikrPkVIyqNK7w7is2uruTAmtPIYM_Pgqw3eUPshVgsc0-VB7IBuy8hNBWTAIw3W952zQfRhot2jAecCDH70rRb_CpzQkQOKLNSSukk6k7D4lmub_OllDjNfp34tf1svckf",
+"expirationTime":null,
+"keys":{"p256dh":"BJovBQ7yPyn5jIywUTY1S1qEsKJScbZp9Hnb0feGFcwwV8PNxhknAJ5QPIhRlR0_k5gWNHl0Yqwnl6LiCcwKnts",
+"auth":"JK5uUic6jmOI6DJ7bo8AyA"}};
 
 push.sendNotification(sub, 'test message')
